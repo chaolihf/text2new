7e9757be415b97840d64c530c6fde08960371e01
chaoli <lichao01.ah@chinatelecom.cn>
2025-03-30 17:06:22 +0800

    feat: 添加文本与JSON相互转换功能
    - 新增两个命令：文本转JSON和JSON转文本
    - 在右键菜单中添加相关选项
    - 更新package.json文件，增加扩展信息和依赖
    - 添加LICENSE.txt文件，使用Apache 2.0开源协议

--- src/extension.ts
+++ src/extension.ts
@@ -1,36 +1,44 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "chaoli" is now active!');
+	context.subscriptions.push(
+		vscode.commands.registerCommand('extension.text2Json', () => {
+			const editor = vscode.window.activeTextEditor;
+			if (editor) {
+				const document = editor.document;
+				const selection = editor.selection;
+				if (selection){
+					const text = document.getText(selection);
+					const jsonText = JSON.stringify(text);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('extension.uppercaseText', () => {
-		const editor = vscode.window.activeTextEditor;
-        if (editor) {
-            const document = editor.document;
-            const selection = editor.selection;
+					editor.edit(editBuilder => {
+						editBuilder.replace(selection, jsonText);
+					});
+				}
+			}
+		})
+	);
 
-            const text = document.getText(selection);
-            const upperCaseText = text.toUpperCase();
 
-            editor.edit(editBuilder => {
-                editBuilder.replace(selection, upperCaseText);
-            });
-        }
-		vscode.window.showInformationMessage('Hello World from text2new!');
-	});
+	context.subscriptions.push( 
+		vscode.commands.registerCommand('extension.json2Text', () => {
+			const editor = vscode.window.activeTextEditor;
+			if (editor) {
+				const document = editor.document;
+				const selection = editor.selection;
+				if (selection){
+					const jsonText = document.getText(selection);
+					const text = JSON.parse(jsonText);
 
-	context.subscriptions.push(disposable);
+					editor.edit(editBuilder => {
+						editBuilder.replace(selection, text);
+					});
+				}
+			}
+		})
+	);
+
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
